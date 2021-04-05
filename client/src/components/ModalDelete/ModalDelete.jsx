44ea6d0825cb85195a1cfcf80289ce2d5a91c621
Bilal Khundmiri <khundmirib@gmail.com>
2021-04-05 17:24:50 -0400

    delete post modal css changes

---
+++ client/src/components/ModalDelete/ModalDelete.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './ModalDelete.css'
+
+function ModalDelete(props) {
+
+    const { open, setOpen, setDeleteOpen, handleDelete } = props;
+
+    return (
+        <div className='modal-delete-container' onClick={(e) => setOpen(false)}>
+            <div className='modal-delete' onClick={(e) => e.stopPropagation()}>
+                <div className='delete-prompt'>
+                    <div>Are you sure you want to delete this post?</div>
+                    <div>This action cannot be undone.</div>
+                </div>
+                <div className='modal-button delete-modal-delete-button' onClick={() => {
+                    handleDelete(open)
+                    setDeleteOpen(false)
+                }}>Delete</div>
+                <div className='modal-button modal-cancel' onClick={() => {
+                    setDeleteOpen(false)
+                    setOpen(false)
+                }}>Cancel</div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default ModalDelete;
--- client/src/screens/PostDetail/PostDetail.jsx
+++ client/src/screens/PostDetail/PostDetail.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
-import { Link, useParams, useHistory } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { destroyPost, getOnePost } from "../../services/posts";
 
 import Post from "../../components/Post/Post";
 
 export default function FoodDetails(props) {
   const [onePost, setOnePost] = useState(null);
-  const [open, setOpen] = useState(false);
 
   const { id } = useParams();
   const history = useHistory();
@@ -32,11 +31,8 @@ export default function FoodDetails(props) {
       <Post
         post={onePost}
         currentUser={currentUser}
-        open={open}
-        setOpen={setOpen}
         handleDelete={handleDelete}
       />
-      
     </>
   ) : (
     <div>Loading...</div>
