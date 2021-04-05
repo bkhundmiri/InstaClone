4577afcf581d026ba0ef1b10e2e7d501ba63f126
Bilal Khundmiri <khundmirib@gmail.com>
2021-04-05 17:31:35 -0400

    code cleanup

--- client/src/components/ProfileBanner/ProfileBanner.jsx
+++ client/src/components/ProfileBanner/ProfileBanner.jsx
@@ -11,26 +11,32 @@ function ProfileBanner(props) {
         <>
             <div className='user-details-container'>
                 <div className='profile-username'>{currentUser?.username}</div>
+
                 <div className='profile-banner-container'>
                     <img className='profile-banner' src={currentUser?.img_url} alt=""/>
+
                     <div className='number-box'>
                         <div className='number'>{oneUserPost?.length}</div>
                         <div>Posts</div>
                     </div>
+
                     <div className='number-box'>
                         <div className='number'>-</div>
                         <div>Followers</div>
                     </div>
+
                     <div className='number-box'>
                         <div className='number'>-</div>
                         <div>Following</div>
                     </div>
                 </div>
+
                 <div className='profile-content'>
                     <p className='user-name'>{currentUser?.full_name}</p>
                     <div>{currentUser?.location}</div>
                     <div>{currentUser?.bio}</div>
                 </div>
+                
                 <Link to='/profile/edit'><button className='edit-profile-button'>Edit Profile</button></Link>
             </div>
         </>
